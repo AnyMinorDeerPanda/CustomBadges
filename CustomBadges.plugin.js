90c79615d77c820f5c2d3656e1b3a56f1516a3cd
AnyMinorDeerPanda <41217280+AnyMinorDeerPanda@users.noreply.github.com>
2020-12-08 22:15:46 -0700

    Created

---
+++ CustomBadges.plugin.js
@@ -0,0 +1,10 @@
+/**
+ * @name CustomBadges
+ * @version 0.0.1
+ * @description Create Custom Badges using BetterDiscord
+ * @author AnyMinorDeerPanda
+ * @invite NJKynJR
+ * @website https://github.com/AnyMinorDeerPanda/
+ * @source https://github.com/AnyMinorDeerPanda/BetterDiscord/blob/main/Themes/CustomBadges/css/source.css
+ * @updateUrl https://anyminordeerpanda.github.io/BetterDiscord/Themes/CustomBadges/css/source.css
+ */
